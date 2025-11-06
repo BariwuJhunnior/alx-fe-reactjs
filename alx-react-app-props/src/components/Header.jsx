9eb6c426f47eeae5c47aa51854bd8c880754081a
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 11:51:11 +0000

    Added UserContext Component

---
+++ alx-react-app-props/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <header>My Favorite Cities</header>;
+}
+
+export default Header;
---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserContext() {
+  return React.createContext();
+}
+
+export default UserContext;
