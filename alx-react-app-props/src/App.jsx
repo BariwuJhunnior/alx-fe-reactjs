afdd4abc5ec4ecf0242009eef70176cd43b75ca9
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 12:14:37 +0000

    more context update

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,6 +5,7 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 import UserContext from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
@@ -16,7 +17,7 @@ function App() {
         <Header />
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <UserContext />
+        <ProfilePage userData={userData} />
         <Footer />
       </UserContext.Provider>
     </>
