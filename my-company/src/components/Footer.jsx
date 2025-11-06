c9633c7714a324f8cd5b7ef536296d179363184e
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 14:16:37 +0000

    component directory renamed

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h6>Copyright@ 2025 My-Company, All rights reserved</h6>
+    </footer>
+  );
+}
+
+export default Footer;
