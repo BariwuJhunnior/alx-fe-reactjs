847bbf3353893e5a999588f202fe9055eea485b4
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 11:17:12 +0000

    Created the Counter Componenet and rendered it in App comp

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Current Count: {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
+
+export default Counter;
