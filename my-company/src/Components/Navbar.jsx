acdfc568a27f09673bb18d7596af4fa29c94fe75
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 13:41:27 +0000

    Added navbar, making it interractive using routes

---
+++ my-company/src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+
+function Navbar() {
+  const { pathname } = useLocation();
+  const isActive = (to) => (pathname === to ? "active" : "");
+
+  return (
+    <nav className="navbar">
+      <div className="brand">MySite</div>
+      <ul className="navlinks">
+        <li>
+          <Link className={isActive("/")} to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={isActive("/about")} to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className={isActive("/services")} to="/services">
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link className={isActive("/contact")} to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
