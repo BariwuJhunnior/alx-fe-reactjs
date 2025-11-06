c647f127c36247fb3473bfd498a64c60e087a7b4
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-06 12:12:50 +0000

    UserContext content updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,13 +7,18 @@ import UserProfile from "./components/UserProfile";
 import UserContext from "./components/UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
     <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
+      <UserContext.Provider value={userData}>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <UserContext />
+        <Footer />
+      </UserContext.Provider>
     </>
   );
 }
