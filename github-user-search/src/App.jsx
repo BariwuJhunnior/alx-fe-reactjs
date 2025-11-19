9b87d66330d4df624633360ef17e77ba9e736a0e
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-19 20:09:34 +0000

    Added the UserCard to the App.js file as content

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,7 +3,33 @@ import "./App.css";
 import Search from "./components/Search";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchUserData } from "./services/githubService";
-import UserCard from "./components/UserCard";
+
+function UserCard({ user }) {
+  if (!user) {
+    return null;
+  }
+  return (
+    <div className="user-card">
+      <img
+        src={user.avartar_url}
+        alt={`${user.login}'s avatar`}
+        style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+      />
+
+      <h3>{user.name || user.login} </h3>
+      <p>
+        <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+          View GitHub Profile
+        </a>
+      </p>
+      {
+        {
+          /* We could add more details here later */
+        }
+      }
+    </div>
+  );
+}
 
 function SearchPage({ users, searchTerm, isLoading, error, handleSearch }) {
   let content;
