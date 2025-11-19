e34e56b46564b186c0ef9ce9c22f9eb05b5e8c67
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-19 14:07:14 +0000

    Added 'Browser Router' to App.jsx file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -22,32 +23,34 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <h1>GitHub User Search</h1>
+    <BrowserRouter>
+      <div className="app-container">
+        <h1>GitHub User Search</h1>
 
-      {/* Later, we will put our SearchBar component here.
+        {/* Later, we will put our SearchBar component here.
         It will update 'searchTerm'. */}
 
-      <div style={searchInputStylesCont}>
-        <input type="text" placeholder="Enter Search Term" style={styles} />
-        <button>Search</button>
+        <div style={searchInputStylesCont}>
+          <input type="text" placeholder="Enter Search Term" style={styles} />
+          <button>Search</button>
+        </div>
+
+        <main className="results-container">
+          <h2>Results ({users.length} found)</h2>
+          {/* Later, we will map over the 'users' array and display a UserCard for each user. */}
+
+          {users.length === 0 ? (
+            <p>Start searching for a GitHub user!</p>
+          ) : (
+            <p>Displaying user cards...</p>
+          )}
+        </main>
+
+        <footer>
+          <p>&copy; 2025 GitHub Search App</p>
+        </footer>
       </div>
-
-      <main className="results-container">
-        <h2>Results ({users.length} found)</h2>
-        {/* Later, we will map over the 'users' array and display a UserCard for each user. */}
-
-        {users.length === 0 ? (
-          <p>Start searching for a GitHub user!</p>
-        ) : (
-          <p>Displaying user cards...</p>
-        )}
-      </main>
-
-      <footer>
-        <p>&copy; 2025 GitHub Search App</p>
-      </footer>
-    </div>
+    </BrowserRouter>
   );
 }
 
