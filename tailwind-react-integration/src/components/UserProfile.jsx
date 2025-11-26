c69e21d4541618031b7a2dea457d38f85f759fe7
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-26 20:14:43 +0000

    Added tailwindcss classes for screen adaptability

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-8 max-w-100 max-h-100 max-w-sm-100 mx-w-md-100 mx-auto my-20 rounded-lg shadow-lg sm:p-16 md:p-18 ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-20 h-20 mx-auto bg-blue-500 sm:w-23 sm:h-23 md:w-25 md:h-25"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 sm:text-xl md:text-2xl md:font-bold">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
