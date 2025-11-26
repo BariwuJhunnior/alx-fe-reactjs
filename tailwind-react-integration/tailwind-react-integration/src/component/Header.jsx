2c2ec44bf5a2aeb7550b31d5c9f9d1de1933205f
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-26 15:25:52 +0000

    added more content

--- tailwind-react-integration/tailwind-react-integration/src/component/Header.jsx
+++ tailwind-react-integration/tailwind-react-integration/src/component/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is the Header</h1>
+    </div>
+  );
 }
 
 export default Header;
--- tailwind-react-integration/tailwind-react-integration/src/component/Main.jsx
+++ tailwind-react-integration/tailwind-react-integration/src/component/Main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Main() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This the main page.</h1>
+    </div>
+  );
 }
 
 export default Main;
