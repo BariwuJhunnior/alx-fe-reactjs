e4557afaf3ad3b9b30b9bfaeade84d95227e27f1
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-26 20:33:55 +0000

    Added hover:scale effect

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-5 max-w-xs max-h-100 max-w-sm-100 mx-w-md-100 mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:max-w-sm md:p-8 text-lg md:text-xl text-sm hover:shadow-xl">
+    <div className="bg-gray-100 p-5 max-w-xs max-h-100 max-w-sm-100 mx-w-md-100 mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:max-w-sm md:p-8 text-lg md:text-xl text-sm hover:shadow-xl ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
