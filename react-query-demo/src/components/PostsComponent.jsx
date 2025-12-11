bdeacd93b89ea2cf1b8a69332fcd95d910dd0142
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-11 15:52:11 +0000

    Advanced Data Handlin with React Query

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+function PostsComponent() {
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    return response.json();
+  };
+
+  const {data, error, isLoading} = useQuery('fetchData', fetchData);
+
+  //Handle isLoading State
+  if (isLoading) {
+    return <div>Loading....</div>
+  }
+
+  //Handle Error State
+  if (error) {
+    return <div>Error Loading Data!</div>
+  }
+
+  //Render the fetched Data
+  return (
+    <div>
+      {data.map(item => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default PostsComponent
