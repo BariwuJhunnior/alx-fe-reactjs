1e1e4e351701637422c13248359e524db02934a2
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-11 16:16:02 +0000

    Implementation of refetch button

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -11,15 +11,15 @@ function PostsComponent() {
   };
 
   const {data, error, isLoading, isError, fetchPosts} = useQuery({
-    queryKey: 'fetchData',
-    queryFn: fetchData,
-    //Add the options the linter is requiring (even if they use the defaults)
-    cacheTime: 5 * 60 * 1000,
-    staleTime: 0, // Default: 0 seconds (data is immediatly stale)
-    refetchOnWindowFocus: true, //Default: true
-    //Note: keepPreviousData is often used for pagination/search, so the linter might justw want it there.
-    keepPreviousData: false,
-});
+      queryKey: ['fetchData'],
+      queryFn: fetchData,
+      //Add the options the linter is requiring (even if they use the defaults)
+      cacheTime: 5 * 60 * 1000,
+      staleTime: 0, // Default: 0 seconds (data is immediatly stale)
+      refetchOnWindowFocus: true, //Default: true
+      //Note: keepPreviousData is often used for pagination/search, so the linter might justw want it there.
+      keepPreviousData: false,
+  });
 
   //Handle isLoading State
   if (isLoading) {
@@ -34,6 +34,15 @@ function PostsComponent() {
   //Render the fetched Data
   return (
     <div>
+      <h2>Post Lists</h2>
+      {/* Implement the required 'button' and 'onClick' interaction */}
+      
+      <button onClick={() => {
+        fetchData()
+      }}>
+        {isLoading ? 'Refreshing' : 'Refetch'}
+      </button>
+
       {data.map(item => (
         <div key={item.id}>{item.name}</div>
       ))}
